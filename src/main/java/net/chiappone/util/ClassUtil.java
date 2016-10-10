f8dfa9bbb762f5c2c0098101ed72bb0850f4b251
Kurtis Chiappone <kchiappone@gmail.com>
2016-10-10 15:29:33 -0400

    Formatting

--- src/main/java/net/chiappone/util/ClassUtil.java
+++ src/main/java/net/chiappone/util/ClassUtil.java
@@ -11,140 +11,139 @@ import java.util.Set;
  */
 public class ClassUtil {
 
-	private static final Set<String> collectionTypes = new HashSet<String>();
-	private static final Set<String> primitiveTypes = new HashSet<String>();
+    private static final Set<String> collectionTypes = new HashSet<String>();
+    private static final Set<String> primitiveTypes = new HashSet<String>();
 
-	static {
+    static {
 
-		// Collection Interfaces
-		collectionTypes.addAll(
-				Arrays.asList( "Collection", "BeanContext", "BeanContextServices", "BlockingQueue",
-						"List", "Queue", "Set", "SortedSet" ) );
+        // Collection Interfaces
+        collectionTypes.addAll(
+                        Arrays.asList( "Collection", "BeanContext", "BeanContextServices", "BlockingQueue", "List",
+                                        "Queue", "Set", "SortedSet" ) );
 
-		// Collection Implementations
-		collectionTypes.addAll(
-				Arrays.asList( "AbstractCollection", "AbstractList", "AbstractQueue",
-						"AbstractSequentialList", "AbstractSet", "ArrayBlockingQueue", "ArrayList",
-						"AttributeList", "BeanContextServicesSupport", "ConcurrentLinkedQueue",
-						"CopyOnWriteArrayList", "CopyOnWriteArraySet", "DelayQueue", "EnumSet",
-						"HashSet", "JobStateReasons", "LinkedBlockingQueue", "LinkedHashSet",
-						"LinkedList", "PriorityBlockingQueue", "PriorityQueue", "RoleList",
-						"RoleUnresolvedList", "Stack", "SynchronousQueue", "TreeSet", "Vector" ) );
+        // Collection Implementations
+        collectionTypes.addAll(
+                        Arrays.asList( "AbstractCollection", "AbstractList", "AbstractQueue", "AbstractSequentialList",
+                                        "AbstractSet", "ArrayBlockingQueue", "ArrayList", "AttributeList",
+                                        "BeanContextServicesSupport", "ConcurrentLinkedQueue", "CopyOnWriteArrayList",
+                                        "CopyOnWriteArraySet", "DelayQueue", "EnumSet", "HashSet", "JobStateReasons",
+                                        "LinkedBlockingQueue", "LinkedHashSet", "LinkedList", "PriorityBlockingQueue",
+                                        "PriorityQueue", "RoleList", "RoleUnresolvedList", "Stack", "SynchronousQueue",
+                                        "TreeSet", "Vector" ) );
 
-		// Primitives
-		primitiveTypes.addAll(
-				Arrays.asList( "byte", "char", "character", "int", "integer", "double", "float",
-						"long", "short", "boolean", "string" ) );
+        // Primitives
+        primitiveTypes.addAll( Arrays.asList( "byte", "char", "character", "int", "integer", "double", "float", "long",
+                        "short", "boolean", "string" ) );
 
-	}
+    }
 
-	/**
-	 * Returns the class name from the fully-qualified name.
-	 *
-	 * @param fullyQualifiedName
-	 * @return String
-	 */
-	public static String getClassName( String fullyQualifiedName ) {
+    /**
+     * Returns the class name from the fully-qualified name.
+     *
+     * @param fullyQualifiedName
+     * @return String
+     */
+    public static String getClassName( String fullyQualifiedName ) {
 
-		return fullyQualifiedName.substring( fullyQualifiedName.lastIndexOf( "." ) + 1 );
+        return fullyQualifiedName.substring( fullyQualifiedName.lastIndexOf( "." ) + 1 );
 
-	}
+    }
 
-	/**
-	 * Returns the fully-qualified class name taken from a file and package.
-	 *
-	 * @param javaFile
-	 * @param packageName
-	 * @return String
-	 */
-	public static String getFullyQualifiedName( File javaFile, String packageName ) {
+    /**
+     * Returns the fully-qualified class name taken from a file and package.
+     *
+     * @param javaFile
+     * @param packageName
+     * @return String
+     */
+    public static String getFullyQualifiedName( File javaFile, String packageName ) {
 
-		String name = javaFile.getName();
-		return packageName + "." + name.substring( 0, name.indexOf( "." ) );
+        String name = javaFile.getName();
+        return packageName + "." + name.substring( 0, name.indexOf( "." ) );
 
-	}
+    }
 
-	/**
-	 * Extracts a parameterization. Returns null if the passed in String does
-	 * not contain a parameterization.
-	 *
-	 * @param parameterizedClass
-	 * @return String
-	 */
-	public static String extractParameterization( String parameterizedClass ) {
+    /**
+     * Extracts a parameterization. Returns null if the passed in String does
+     * not contain a parameterization.
+     *
+     * @param parameterizedClass
+     * @return String
+     */
+    public static String extractParameterization( String parameterizedClass ) {
 
-		if ( !isParameterized( parameterizedClass ) )
-			return null;
+        if ( !isParameterized( parameterizedClass ) )
+            return null;
 
-		return parameterizedClass.substring( parameterizedClass.indexOf( "<" ) + 1,
-				parameterizedClass.lastIndexOf( ">" ) );
+        return parameterizedClass
+                        .substring( parameterizedClass.indexOf( "<" ) + 1, parameterizedClass.lastIndexOf( ">" ) );
 
-	}
+    }
 
-	/**
-	 * Determines if the passed in String representation of a class is a
-	 * Collection type.
-	 *
-	 * @param clazz
-	 * @return boolean
-	 */
-	public static boolean isCollection( String clazz ) {
+    /**
+     * Determines if the passed in String representation of a class is a
+     * Collection type.
+     *
+     * @param clazz
+     * @return boolean
+     */
+    public static boolean isCollection( String clazz ) {
 
-		if ( isParameterized( clazz ) ) {
-			clazz = removeParameterization( clazz );
-		}
+        if ( isParameterized( clazz ) ) {
+            clazz = removeParameterization( clazz );
+        }
 
-		if ( clazz.contains( "java.util." ) ) {
-			clazz = clazz.substring( 10 );
-		}
+        if ( clazz.contains( "java.util." ) ) {
+            clazz = clazz.substring( 10 );
+        }
 
-		return collectionTypes.contains( clazz );
+        return collectionTypes.contains( clazz );
 
-	}
+    }
 
-	/**
-	 * Determines if the passed in String representation of a class is
-	 * parameterized.
-	 *
-	 * @param clazz
-	 * @return boolean
-	 */
-	public static boolean isParameterized( String clazz ) {
+    /**
+     * Determines if the passed in String representation of a class is
+     * parameterized.
+     *
+     * @param clazz
+     * @return boolean
+     */
+    public static boolean isParameterized( String clazz ) {
 
-		return clazz != null && clazz.contains( "<" ) && clazz.contains( ">" );
+        return clazz != null && clazz.contains( "<" ) && clazz.contains( ">" );
 
-	}
+    }
 
-	/**
-	 * Determines if the passed in String representation of a class is a
-	 * primitive type or a primitive type wrapper.
-	 *
-	 * @param clazz
-	 * @return boolean
-	 */
-	public static boolean isPrimitiveType( String clazz ) {
+    /**
+     * Determines if the passed in String representation of a class is a
+     * primitive type or a primitive type wrapper.
+     *
+     * @param clazz
+     * @return boolean
+     */
+    public static boolean isPrimitiveType( String clazz ) {
 
-		clazz = clazz.toLowerCase();
+        clazz = clazz.toLowerCase();
 
-		if ( clazz.contains( "java.lang." ) ) {
-			clazz = clazz.substring( 10 );
-		}
+        if ( clazz.contains( "java.lang." ) ) {
+            clazz = clazz.substring( 10 );
+        }
 
-		return primitiveTypes.contains( clazz );
+        return primitiveTypes.contains( clazz );
 
-	}
+    }
 
-	/**
-	 * Removes the parameterized type from the given String representation of a
-	 * class.
-	 *
-	 * @param clazz
-	 * @return String
-	 */
-	public static String removeParameterization( String clazz ) {
+    /**
+     * Removes the parameterized type from the given String representation of a
+     * class.
+     *
+     * @param clazz
+     * @return String
+     */
+    public static String removeParameterization( String clazz ) {
 
-		return clazz.substring( 0, clazz.indexOf( "<" ) );
+        return clazz.substring( 0, clazz.indexOf( "<" ) );
 
-	}
+    }
 
 }
